f219520bc30621d6eff02cdbabb514e9e6dfdb9f
若林勇汰 <wnyuukure@gmail.com>
2019-08-29 11:54:14 +0900

    [add] ProductDetails画面に取ってきたデータを表示

--- src/components/ProductDetails/ProductDetails.tsx
+++ src/components/ProductDetails/ProductDetails.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
 import Props from './ProductDetailsProps';
+import { withStyles } from '@material-ui/styles';
+import styles from './ProductDetailsStyles';
+import { Typography } from '@material-ui/core';
 
 class ProductDetails extends React.Component<Props> {
   render() {
-    const { productDetails } = this.props;
-    return <div>{productDetails.name}</div>;
+    const { classes, productDetails } = this.props;
+    return (
+      <div className={classes.container}>
+        <img className={classes.image} alt='complex' src={productDetails.icon_path} />
+        <div className={classes.description}>
+          <Typography className={classes.madeBy} variant='subtitle1' color='textSecondary'>
+            {productDetails.made_by}
+          </Typography>
+          <Typography variant='h2'>{productDetails.name}</Typography>
+          <Typography
+            className={classes.value}
+            variant='h4'
+          >{`¥ ${productDetails.value}`}</Typography>
+          <Typography
+            className={classes.reviewPoint}
+            variant='subtitle1'
+          >{`評価：${productDetails.review_point}`}</Typography>
+          <Typography variant='subtitle1'>{`コメント：${productDetails.review_content}`}</Typography>
+        </div>
+      </div>
+    );
   }
 }
 
-export default ProductDetails;
+export default withStyles(styles)(ProductDetails);
--- src/components/ProductDetails/ProductDetailsStyles.ts
+++ src/components/ProductDetails/ProductDetailsStyles.ts
@@ -1,5 +1,27 @@
 import { Theme, createStyles } from '@material-ui/core';
 
-const styles = (theme: Theme) => createStyles({});
+const styles = (theme: Theme) =>
+  createStyles({
+    container: {
+      display: 'flex',
+      flexDirection: 'row',
+    },
+    image: {
+      width: '50%',
+      height: '100%',
+    },
+    description: {
+      textAlign: 'left',
+    },
+    madeBy: {
+      marginTop: 120,
+    },
+    value: {
+      marginTop: 40,
+    },
+    reviewPoint: {
+      marginTop: 40,
+    },
+  });
 
 export default styles;
